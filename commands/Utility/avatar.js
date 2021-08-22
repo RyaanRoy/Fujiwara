a565666b245691cbf3309293ba56bd7c6ca09562
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-08-22 23:01:34 +0530

    ok
    ok

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -12,7 +12,7 @@ module.exports.run = (client, message, args) => {
 
 	const embed = new Discord.MessageEmbed()
 		.setColor("GREEN")
-		.setTitle(`Avatar of ${message.mentions.users.first().username}:`)
+		.setTitle(`Avatar`)
 		.setImage(
 			imageitem.displayAvatarURL({ dynamic: false, format: "png", size: 4096 })
 		);
@@ -23,7 +23,7 @@ module.exports.run = (client, message, args) => {
 module.exports.help = {
 	name: "avatar",
 	description: "This command is used for showing your/other member's avatar.",
-	usage: "z!avatar <mentions>(optional)",
+	usage: "b-avatar <mentions>(optional)",
 	accessableby: "Member",
 	aliases: ["av"]
 };

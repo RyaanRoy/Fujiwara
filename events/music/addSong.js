b9eea8bb995a75fab664d91decdf80f2ad65c1a0
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-08-23 21:36:16 +0530

    ok
    ok

--- events/music/addSong.js
+++ events/music/addSong.js
@@ -1,11 +1,5 @@
 module.exports = async (client, message, queue, song) => {
-	const Embed = new Discord.MessageEmbed()
-    .setAuthor(`New Song Added`, gifmusic)
-    .setTitle(`Queue Updated`)
-    .setDescription(`| Added ${song.name} - \`${song.formattedDuration}\` to the queue`) 
-    .setColor(`YELLOW`)
-    .setThumbnail(`${song.thumbnail}`);
-	message.channel.send(Embed);
+	message.channel.send(
+		`Added ${song.name} - \`${song.formattedDuration}\` to the queue`
+	);
 };
-	
-

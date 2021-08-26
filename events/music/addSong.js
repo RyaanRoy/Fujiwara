a0e6086efdb8ea061e98d9a311b7fecbffcd8ead
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-08-26 22:42:43 +0530

    hmm
    ok

--- events/music/addSong.js
+++ events/music/addSong.js
@@ -1,9 +1,4 @@
 module.exports = async (client, message, queue, song) => {
-	if (message.member.voice.channel!==client.voice.channel){
-	return message.channel.send(
-		`${client.emotes.error} | You must be in same voice channel as Bumblebee`
-	);
-	}
 	message.channel.send(
 		`Added ${song.name} - \`${song.formattedDuration}\` to the queue`
 	);

00816ea33c0dfb3419c549fa0040edbdd772fb42
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-19 22:13:10 +0530

    ok
    ok

---
+++ commands/owner/eval.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const config = require("../../config/config.json");
+
+module.exports.run = async (client, message, args) => {
+	if (message.author.id != process.env.OWNERID){
+		return message.channel.send("Only my developer can use this command...");
+    }
+        const code = args.join(" ");
+        if (!code)
+          return message.channel.send("Please provide some code to evaluate!");
+    
+        try {
+          const result = await eval(code);
+          let output = result;
+    
+          if (typeof result !== "string") {
+            output = inspect(result);
+          }
+    
+          message.channel.send(
+            new Discord.MessageEmbed()
+              .setColor("#00FF00")
+              .setTitle(`<:tickYes:863367013464408084> | 200 : Success`)
+              .setDescription(`Results\n\`\`\`yml\n${output}\n\`\`\``)
+              .setFooter(`Actioned by : ${message.author.tag}`)
+          );
+        } catch (error) {
+          console.log(error);
+        };
+
+};
+
+module.exports.help = {
+	name: "eval",
+	description:
+		"This command is used for evaluating.",
+	usage: "b-eval[code]",
+	accessableby: "Bot Owners",
+	aliases: []
+};

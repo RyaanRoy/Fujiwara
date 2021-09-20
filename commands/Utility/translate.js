0abe1c88c9120764ccc438cc207957c5d389e43d
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-20 16:24:36 +0530

    ok
    ok

--- commands/Utility/translate.js
+++ commands/Utility/translate.js
@@ -4,14 +4,14 @@ const { Client, Message, MessageEmbed } = require("discord.js");
 const translate = require("@iamtraction/google-translate");
 module.exports.run = async (client, message, args) => {
     const query = args.join(" ");
-    if (!query) return message.lineReply("Please specify a text to translate!");
+    if (!query) return message.channel.send("Please specify a text to translate!");
 
     const translated = await translate(query, { to: "en" });
 
     const embed = new MessageEmbed()
       .setAuthor(
         message.author.tag,
-        message.author.displayAvatarURL({ dynamic: false })
+        message.author.displayAvatarURL({ dynamic: true })
       )
       .addField("Query", query)
       .addField("Result", translated.text)

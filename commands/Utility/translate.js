0670e1f0bdba17654e1b41023c64fdab9d0f18a9
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-20 16:20:46 +0530

    ok
    ok

--- commands/Utility/translate.js
+++ commands/Utility/translate.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const request = require("request");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const translate = require("@iamtraction/google-translate");
-module.exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     const query = args.join(" ");
     if (!query) return message.lineReply("Please specify a text to translate!");
 

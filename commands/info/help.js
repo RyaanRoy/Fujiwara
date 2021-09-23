d335deebfd86526f326453aadb58e9c64f6b136b
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-23 12:20:45 +0530

    ok
    ok

--- commands/info/help.js
+++ commands/info/help.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
 			`Yoo! **My prefix is:** \`${prefixesdatabase.prefix}\` \nClick [HERE](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands) to invite me to your server.\nTo chat with me simply mention or reply to me and ask a question.\n📱Basic: help, ping, uptime, vote`
 		)
 		.addField(`> <a:purplestar:817788092768976896> Moderation`, `clear, ban, unban, warn, clearwarn, warnings, kick, createchannel, createemoji, lockchannel, unlockchannel, mute, unmute, rename, slowmode`, true)
-		.addField(`> <a:purplestar:817788092768976896> Fun`, `8ball, ship, animesearch, aes256, deaes256, meme,`, true)
+		.addField(`> <a:purplestar:817788092768976896> Fun`, `8ball, ship, animesearch, aes256, deaes256, meme`, true)
 		.addField(`> <:blurpleannouncements:859068819191496734> GiveAway`, `start-giveaway , reroll, end-giveaway`, false)
 		.addField(`> <a:dev:817788400471638016> Utility`, `enlarge, reminder, wikipedia, urban, roblox, serverinfo, serverav, avatar, whois, roleinfo, channel, embed(use ;), imageembed, reverse, setafk, snipe, stats, timer, translate, weather`, false)
 		.addField(`> 🖼️ Image`, `captcha, circle, delete, think, gay, changemymind, trigger, clyde, petpet, magik, dog, cat, drake, rip, iphonex, ngif`, false)

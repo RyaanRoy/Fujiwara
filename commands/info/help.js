53d1f75363033bfbe191ea8b15e9df871bf19bc6
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-23 11:49:33 +0530

    ok
    ok

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const [ CModeration, CFun, CGiveaway, CUtility, CImage, CMusic, CGames, CRp ] = require("../../commands/info/allcommands");
+const [ CModeration, CFun, CGiveaway, CUtility, CImage, CMusic, CGames, CRp ] = require("../../commands/info/allcommands.js");
 module.exports.run = async (client, message, args) => {
 	const settings = require("../../config/settings.json");
 	const prefixesdatabase = client.settings.ensure(message.guild.id, settings);

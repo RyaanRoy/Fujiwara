5bfde514b8bb0450e1d9c248523c40363421d318
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-26 23:45:38 +0530

    ok
    ok

--- events/music/playSong.js
+++ events/music/playSong.js
@@ -10,7 +10,7 @@ module.exports = async (client, message, queue, song) => {
        .addField(`Options`,
             `${client.status(queue)}`
     )
-    .setColor(`PINK`)
+    .setColor(`#f77ec6`)
     .setThumbnail(`${song.thumbnail}`);
 	message.channel.send(Embed);
 };

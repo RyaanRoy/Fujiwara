5abc6264e3ea48c14ab2787071828c12b862fa41
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-28 20:29:30 +0530

    ok
    ok

--- commands/Music/pause.js
+++ commands/Music/pause.js
@@ -3,10 +3,6 @@ module.exports.run = async (client, message, args) => {
 		return message.channel.send(
 			`${client.emotes.error} | You must be in a voice channel!`
 		);
-	if (!client.distube.isPlaying(message))
-		return message.channel.send(
-			`${client.emotes.error} | There is nothing playing!`
-		);
 	client.distube.pause(message);
 	message.channel.send(`${client.emotes.success} | Paused!`);
 };

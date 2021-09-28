f411f3ab75424a6c001922239ecde390c5bb0a42
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-28 11:35:19 +0530

    ok
    ok

--- events/music/playSong.js
+++ events/music/playSong.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const gifmusic="https://cdn.discordapp.com/attachments/850619329628471336/877233598057545749/image_processing20200131-9914-t3yrnb_1.gif";
 module.exports = async (client, message, queue, song) => {
 	const Embed = new Discord.MessageEmbed()
-    .setAuthor(`<a:disk:855561346087387136> Now Playing`)
+    .setAuthor(`Now Playing`, gifmusic)
     .setTitle(`${song.name} - ${
         song.formattedDuration
     }`)

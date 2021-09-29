558e6830768b0773a3956944a7262b6533a61e4b
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-29 12:02:29 +0530

    ok
    ok

---
+++ commands/fun/calculator.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const { Client, Message, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const moment = require("moment");
+const { Calculator } = require('weky');
+module.exports.run = async (client, message, args) => {
+    await Calculator({
+        message: message,
+        embed: {
+            title: 'Calculator',
+            color: '#5865F2',
+            footer: '©️ Fujiwara Calculator',
+            timestamp: true,
+        },
+        disabledQuery: 'Calculator is disabled!',
+        invalidQuery: 'The provided equation is invalid!',
+        othersMessage: 'Only <@{{author}}> can use the buttons!',
+    });
+};
+
+module.exports.help = {
+	name: "calculator",
+	description:
+		"calculator on discord",
+	usage: "b-calculator",
+	accessableby: "Member",
+	aliases: ["clt"]
+};
--- commands/info/allcommands.js
+++ commands/info/allcommands.js
@@ -53,6 +53,7 @@ const CGames = [
     "\`youtubetogether\` ",
     "\`betrayal\` ",
     "\`trivia\` ",
+    "\`calculator\` ",
     
 ]
 

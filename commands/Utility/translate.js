23fab1622ba8a31f27b444abc6a384c37f0da2fe
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-09-30 11:23:18 +0530

    ok
    ok

--- commands/Utility/translate.js
+++ commands/Utility/translate.js
@@ -3,23 +3,23 @@ const request = require("request");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const translate = require("@iamtraction/google-translate");
 module.exports.run = async (client, message, args) => {
-    const query = args.join(" ");
-    if (!query) return message.channel.send("Please specify a text to translate!");
+  const query = args.join(" ");
+  if (!query) return message.lineReply("Please specify a text to translate!");
 
-    const translated = await translate(query, { to: "en" });
+  const translated = await translate(query, { to: "en" });
 
-    const embed = new MessageEmbed()
-      .setAuthor(
-        message.author.tag,
-        message.author.displayAvatarURL({ dynamic: true })
-      )
-      .addField("Query", query)
-      .addField("Result", translated.text)
-      .addField("Translated to", "English")
-      .setColor("BLUE")
-      .setFooter(message.author.tag)
-      .setTimestamp();
-    message.channel.send(embed);
+  const embed = new MessageEmbed()
+    .setAuthor(
+      message.author.tag,
+      message.author.displayAvatarURL({ dynamic: true })
+    )
+    .addField("Query", query)
+    .addField("Result", translated.text)
+    .addField("Translated to", "English")
+    .setColor("PINK")
+    .setFooter(message.author.tag)
+    .setTimestamp();
+  message.lineReplyNoMention(embed);
 };
 
 module.exports.help = {

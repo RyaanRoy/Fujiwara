ac0ef5ad6cb80194b138034bd080dc1cf9aafd1d
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-10-02 10:25:39 +0530

    o
    k

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ client.on("message", async message => {
 			  
 			  
 				
-				  message.lineReply(`${Discord.Util.removeMentions(data.cnt)}`);
+				  message.lineReply(`${data.content}`);
 				 
 				 
 			 

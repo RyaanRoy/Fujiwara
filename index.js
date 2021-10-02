79f72934e8cd8baf9e9eda98d76f29bb5994331a
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-10-02 10:35:29 +0530

    ok
    ok

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ client.on("message", async message => {
 			fetch.default(`http://api.brainshop.ai/get?bid=159771&key=5RopgmgY4hFiKTEj&uid=${message.author.id}&msg=${encodeURIComponent(message.content)}`)
 			.then((res) => res.json())
 			.then((body) => {
-				  message.lineReply(`${body.response}`); 
+				message.reply(`${Discord.Util.removeMentions(body.response)}`)
 		  })
 		  }
 		

5785855f29f5f1e59c37341006a9f1926e4e8285
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-10-04 15:03:27 +0530

    ok
    ok

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
 		);
 	const queue = client.distube.skip(message);
 	message.channel.send(
-		`<a:yup:871815263153172550>  | Skipped:\n${queue.songs[0].name}`
+		`<a:yup:871815263153172550> | Skipped:${queue.songs[0].name}`
 	);
 };
 

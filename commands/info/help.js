348b1b6e15e527d2756f41f9f97b7c864e7c38db
Ryaan <ryaanroy43@gmail.com>
2021-10-12 13:26:00 +0530

    Revert "ok"
    This reverts commit a44c46011f3c894cb63b573cef131c5715e74551.

--- commands/info/help.js
+++ commands/info/help.js
@@ -21,7 +21,7 @@ module.exports.run = async (client, message, args) => {
 		.addField(`> 🖼️ Image`, `captcha, circle, delete, think, gay, changemymind, trigger, clyde, petpet, magik, dog, cat, drake, rip, iphonex`, false)
 		.addField(`> <a:chikadance:852764676429185035>  Games`, `akinator, youtubetogether, betrayal, trivia, calculator`, true)
 		.addField(`> <:mm:848450657614037002>  Roleplay`, `kiss, hug, pat, poke, smug, tickle, slap, feed, cuddle, poke, meow, baka, waifu `, true)
-		.addField(`> <a:dance:897022592559484968> Music`, `play, pause, stop, skip, queue, autoplay, loop, volume, resume, lyrics, filter, jumpto`, false)
+		.addField(`> <a:disk:855561346087387136> Music`, `play, pause, stop, skip, queue, autoplay, loop, volume, resume, lyrics, filter, jumpto`, false)
 		.setColor('#cc338b')
 		.setImage(`https://i.pinimg.com/originals/b6/b4/de/b6b4ded4bd797b093cc9b68aa6fba694.gif`)
 		.setFooter(`To set Custom prefix type: ${prefixesdatabase.prefix}setprefix`, client.user.displayAvatarURL())

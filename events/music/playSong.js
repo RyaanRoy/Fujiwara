587b4f59645fe34ec6c90d813d27140adb08abff
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-10-28 22:52:33 +0530

    ok
    ok

--- events/music/playSong.js
+++ events/music/playSong.js
@@ -1,3 +1,4 @@
+var shortNumber = require('short-number');
 const Discord = require("discord.js");
 const gifmusic="https://cdn.discordapp.com/attachments/850619329628471336/877233598057545749/image_processing20200131-9914-t3yrnb_1.gif";
 module.exports = async (client, message, queue, song) => {
@@ -7,8 +8,9 @@ module.exports = async (client, message, queue, song) => {
         song.formattedDuration
     }`)
     .setDescription(`Requested by: ${song.user.tag}`) 
-    .addField(`> <:5618like:865325952866058240> **Likes**`, `   **    ${queue.songs[0].likes}**`, true)
-.addField(`> <:dislike:865325952811401276> **Dislikes**`, `   **    ${queue.songs[0].dislikes}**`, true)
+    .addField(`> <:5618like:865325952866058240> **Likes**`, `   **    ${shortNumber(queue.songs[0].likes)}**`, true)
+.addField(`> <:dislike:865325952811401276> **Dislikes**`, `   **    ${shortNumber(queue.songs[0].dislikes)}**`, true)
+.addField(`> <:eyes_sus:843496570174308352>  **Views**`, `   **    ${shortNumber(queue.songs[0].views)}**`, true)
        .addField(`Options`,
 `${client.status(queue)}`, false)
     .setImage(`https://media.tenor.co/videos/573252ab1b3671a492ede5e985eb3a24/mp4`)

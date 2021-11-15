2bd3c4b3df6f733aa488efec3373c27fa967c0a8
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-11-15 10:53:22 +0530

    ok
    ok

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ client.on("message", async message => {
 	
 		  if (message.mentions.has(client.user.id) && !message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))){
 			try {
-			fetch(`http://api.brainshop.ai/get?bid=159771&key=5RopgmgY4hFiKTEj&uid=${message.author.id}&msg=${encodeURIComponent(message.content)}`)
+			fetch(`http://api.brainshop.ai/get?bid=160117&key=AmxzVOo74jyHpdxp&uid=${message.author.id}&msg=${encodeURIComponent(message.content)}`)
 				.then((res) => res.json())
 				.then((body) => {
 					

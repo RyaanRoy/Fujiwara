87caa37e5b8c1cce69510c533c2d3cd5f40fae98
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-12-02 12:12:46 +0530

    ok
    ok

--- commands/owner/newhelp.js
+++ commands/owner/newhelp.js
@@ -15,14 +15,14 @@ module.exports.run = async (client, message, args) => {
     .addField(`<a:duckdance:859068191871598592>   Support Server`, `To join the support server: [Invite to support server](https://discord.gg/qXDyWEesW6)`, false)
     .addField(`Annoying Bugs`,`See a bug? Use f-bugreport and describe the bug. It will be reviewed by the bot dev immediately!`, false)
     .addField(`Command Sections`,`
-        >Moderation
-        >Fun
-        >Utility
-        >Giveaways
-        >Image Generation
-        >Games
-        >Roleplay
-        >Music
+    <:arrow:904250175822889010>Moderation
+    <:arrow:904250175822889010>Fun
+    <:arrow:904250175822889010>Utility
+    <:arrow:904250175822889010>Giveaways
+    <:arrow:904250175822889010>Image Generation
+    <:arrow:904250175822889010>Games
+    <:arrow:904250175822889010>Roleplay
+    <:arrow:904250175822889010>Music
     `)
     .setColor(`#cc338b`)
     .setThumbnail(client.user.displayAvatarURL())
@@ -34,21 +34,21 @@ module.exports.run = async (client, message, args) => {
     .setAuthor(`${client.user.username}`, client.user.displayAvatarURL())
     .setTitle(`**Moderation Commands**`)
     .addField(`Total Moderation Commands: 15`,`
-       >Purge
-       >Mute
-       >Unmute
-       >Kick
-       >Ban
-       >Unban
-       >Warn
-       >Warnings
-       >Clearwarn
-       >Createchannel
-       >Createemoji
-       >Lockchannel
-       >Unlockchannel
-       >Rename
-       >Slowmode
+    <:arrow:904250175822889010>Purge
+    <:arrow:904250175822889010>Mute
+    <:arrow:904250175822889010>Unmute
+    <:arrow:904250175822889010>Kick
+    <:arrow:904250175822889010>Ban
+    <:arrow:904250175822889010>Unban
+    <:arrow:904250175822889010>Warn
+    <:arrow:904250175822889010>Warnings
+    <:arrow:904250175822889010>Clearwarn
+    <:arrow:904250175822889010>Createchannel
+    <:arrow:904250175822889010>Createemoji
+    <:arrow:904250175822889010>Lockchannel
+    <:arrow:904250175822889010>Unlockchannel
+    <:arrow:904250175822889010>Rename
+    <:arrow:904250175822889010>Slowmode
 `)
     .setColor(`#cc338b`)
     .setThumbnail(client.user.displayAvatarURL())

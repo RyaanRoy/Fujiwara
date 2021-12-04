0a7f072e408f4befb216ea55bfcaa4b83cff7d70
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-12-04 11:36:02 +0530

    Update anime.js

--- commands/fun/anime.js
+++ commands/fun/anime.js
@@ -2,9 +2,11 @@ const Discord = require("discord.js");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 const moment = require("moment");
+const malScraper = require('mal-scraper');
 module.exports.run = async (client, message, args) => {
-    const query = args.join(" ");
-    if (!query) return message.channel.send("Please specify a query to search!");
+  const search = `${args}`;
+  if(!search){
+  return message.reply('Please add a search query if invalid command will not work.');}
     fetch(`https://api.jikan.moe/v3/search/anime?q=${query}`)
       .then((res) => res.json())
       .then((body) => {
@@ -12,40 +14,32 @@ module.exports.run = async (client, message, args) => {
         const mal_url = body.results[0].url;
         const imgae = body.results[0].image_url;
         const synopsis = body.results[0].synopsis;
-        const type = body.results[0].type;
-        const episode = body.results[0].episodes;
-        const score = body.results[0].score;
-        const start_date = body.results[0].start_date;
-        const rate = body.results[0].rated || "Unknown";
+        malScraper.getInfoFromName(search)
+        .then((data) => {
+        const malEmbed = new Discord.MessageEmbed()
+          .setAuthor(`My Anime List search result for ${args}`.split(',').join(' '))
+          .setThumbnail(data.picture)
+          .setColor('#ffc1cc') //I personally use bubblegum pink!
+          .addField('English Title', data.englishTitle, true)
+          .addField('Japanese Title', data.japaneseTitle, true)
+          .addField('Type', data.type, true)
+          .addField('Episodes', data.episodes, true)
+          .addField('Rating', data.rating, true)
+          .addField('Aired', data.aired, true)
+          .addField('Score', data.score, true)
+          .addField('Score Stats', data.scoreStats, true)
+          .addField('Link', data.url)
+          .setImage(imgae)
+          .setDescription(synopsis);
+         
+          
+          message.channel.send({ embeds: [malEmbed] });
 
-        const embed = new MessageEmbed()
-          .setTitle(title)
-          .setURL(mal_url)
-          .setThumbnail(imgae)
-          .setDescription(synopsis)
-          .addField(`Type`, type)
-          .addField(`Total Episode`, episode)
-          .addField(`Ratings (at MyAnimeList)`, score)
-          .addField(`Released`, moment(start_date).format("LLLL"))
-          .addField(`Rate`, rate)
-          .setColor(`#800080`)
-          .setFooter(
-            `Requested by : ${message.author.tag}`,
-            message.author.displayAvatarURL({ dynamic: true })
-          );
-
-          message.channel.send({embeds:[embed]});
+        })
       })
-      .catch((err) =>
-        message.channel.send({embeds:[
-          new MessageEmbed()
-            .setDescription(
-              `That anime isn't found!\n\n\`\`\`js\n${err}\n\`\`\``
-            )
-            .setColor("RED")
-        ]}
-        )
-      );
+
+
+
 };
 
 module.exports.help = {

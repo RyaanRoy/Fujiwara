96d6834ac0c7943dc667893dc1c2e5871d386a74
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-12-27 12:24:19 +0530

    ok
    ok

--- commands/fun/animecharacter.js
+++ commands/fun/animecharacter.js
@@ -2,18 +2,18 @@ const Discord = require("discord.js");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 const moment = require("moment");
-const {waifu} = require('discord-senpai')
+
 const a = require('acb-api');
 module.exports.run = async (client, message, args) => {
   const search = `${args}`;
   if(!search)
   return message.reply('Please add a search query or command will not work.');
-
-  const animesearched = waifu.search(search);
+  const {waifu} = require('discord-senpai')
+  const anime = waifu.search(search);
 const waifuEmbed= new Discord.MessageEmbed()
 .setAuthor(`Info about ${args}`.split(',').join(' '))
-.setDescription(`The character is ${animesearched.name} from the anime ${animesearched.anime}`)
-.setImage(animesearched.URL);
+.setDescription(`The character is ${anime.name} from the anime ${anime.anime}`)
+.setImage(anime.URL);
 message.channel.send({embeds:[waifuEmbed]})
 
 };

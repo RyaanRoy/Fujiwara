e034ee951656b07f6a47d04544711d6182dc8b19
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-12-27 12:19:43 +0530

    ok
    ok

--- commands/fun/animecharacter.js
+++ commands/fun/animecharacter.js
@@ -2,38 +2,20 @@ const Discord = require("discord.js");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 const moment = require("moment");
+const {waifu} = require('discord-senpai')
 const a = require('acb-api');
 module.exports.run = async (client, message, args) => {
   const search = `${args}`;
   if(!search)
   return message.reply('Please add a search query or command will not work.');
 
-  a.get_character_by_search(search)
-    .then((data) => {
-    const Embed = new Discord.MessageEmbed()
-      .setAuthor(`My Anime List search result for ${args}`.split(',').join(' '))
-      .setThumbnail(data.anime_image)
-      .setDescription(JSON.stringify(data.description))
-      .setColor('#ffc1cc') //I personally use bubblegum pink!
-      .addField('Name', JSON.stringify(data.name), true)
-      .addField('Gender', JSON.stringify(data.gender), true)
-      .addField('Origin', JSON.stringify(data.origin), truefalse)
-      .addField('Anime Name', JSON.stringify(data.anime_name), true)
-      .setImage(data.character_image);
+  const animesearched = waifu.search(search);
+const waifuEmbed= new Discord.MessageEmbed()
+.setAuthor(`Info about ${args}`.split(',').join(' '))
+.setDescription(`The character is ${animesearched.name} from the anime ${animesearched.anime}`)
+.setImage(animesearched.URL);
+message.channel.send({embeds:[waifuEmbed]})
 
-      message.channel.send({ embeds: [Embed] });
-
-    })
-      .catch((err) =>
-        message.channel.send({embeds:[
-          new MessageEmbed()
-            .setDescription(
-              `That character isn't found!\n\n\`\`\`js\n${err}\n\`\`\``
-            )
-            .setColor("#cc338b")
-        ]}
-        )
-      );
 };
 
 module.exports.help = {

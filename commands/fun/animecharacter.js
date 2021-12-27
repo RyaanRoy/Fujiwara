545933d137e5485e3bb4df3013eae320445a5c99
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-12-27 11:32:33 +0530

    ok
    ok

---
+++ commands/fun/animecharacter.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const { Client, Message, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const moment = require("moment");
+
+module.exports.run = async (client, message, args) => {
+  const search = `${args}`;
+  if(!search)
+  return message.reply('Please add a search query if invalid command will not work.');
+
+  malScraper.getInfoFromName(search)
+    .then((data) => {
+    const malEmbed = new Discord.MessageEmbed()
+      .setAuthor(`My Anime List search result for ${args}`.split(',').join(' '))
+      .setThumbnail(data.picture)
+      .setDescription(data.synopsis)
+      .setColor('#ffc1cc') //I personally use bubblegum pink!
+      .addField('English Title', data.englishTitle, true)
+      .addField('Japanese Title', data.japaneseTitle, true)
+      .addField('Type', data.type, true)
+      .addField('Episodes', data.episodes, true)
+      .addField('Rating', data.rating, true)
+      .addField('Aired', data.aired, true)
+      .addField('Score', data.score, true)
+      .addField('Score Stats', data.scoreStats, true)
+      .addField('Link', `[MAL Link](${data.url})`, true);
+
+      message.channel.send({ embeds: [malEmbed] });
+
+    })
+      .catch((err) =>
+        message.channel.send({embeds:[
+          new MessageEmbed()
+            .setDescription(
+              `That anime isn't found!\n\n\`\`\`js\n${err}\n\`\`\``
+            )
+            .setColor("#cc338b")
+        ]}
+        )
+      );
+};
+
+module.exports.help = {
+	name: "anime",
+	description:
+		"Searching anime",
+	usage: "f-anime <query>",
+	accessableby: "Member",
+	aliases: ["animesearch"]
+};
--- commands/fun/owo.js
+++ commands/fun/owo.js
@@ -1,10 +1,19 @@
 const Discord = require("discord.js");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const request = require("superagent");
-const owoify = require('owoify-js').default
+const Uwuifier = require('uwuifier');
+const uwuifier = new Uwuifier({
+    spaces: {
+        faces: 0.5,
+        actions: 0.075,
+        stutters: 0.1
+    },
+    words: 1,
+    exclamations: 1
+});
 module.exports.run = async (client, message, args) => {
     const query = args.join(" ");
-message.reply(`${owoify(`**${message.author.username} says:** ${query}`, 'uwu')}`);
+message.reply(`${uwuifier.uwuifySentence(`**${message.author.username} says:** ${query}`)}`);
 };
 
 module.exports.help = {

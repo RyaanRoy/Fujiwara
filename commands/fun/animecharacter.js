64d370c4955afe00e03cee713be0e14f20bfa7f3
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2021-12-27 12:51:46 +0530

    ok
    ok

--- commands/fun/animecharacter.js
+++ commands/fun/animecharacter.js
@@ -1,21 +1,39 @@
-const Discord = require("discord.js");
+/**const Discord = require("discord.js");
 const { Client, Message, MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 const moment = require("moment");
-
 const a = require('acb-api');
 module.exports.run = async (client, message, args) => {
   const search = `${args}`;
   if(!search)
   return message.reply('Please add a search query or command will not work.');
-  const {waifu} = require('discord-senpai')
-  const anime = waifu.search(search);
-const waifuEmbed= new Discord.MessageEmbed()
-.setAuthor(`Info about ${args}`.split(',').join(' '))
-.setDescription(`The character is ${anime.name} from the anime ${anime.anime}`)
-.setImage(anime.URL);
-message.channel.send({embeds:[waifuEmbed]})
 
+  a.get_character_by_search(search)
+    .then((data) => {
+    const Embed = new Discord.MessageEmbed()
+      .setAuthor(`My Anime List search result for ${args}`.split(',').join(' '))
+      .setThumbnail(res.anime_image)
+      .setDescription(JSON.stringify(res.description))
+      .setColor('#ffc1cc') //I personally use bubblegum pink!
+      .addField('Name', JSON.stringify(res.name), true)
+      .addField('Gender', JSON.stringify(res.gender), true)
+      .addField('Origin', JSON.stringify(res.origin), truefalse)
+      .addField('Anime Name', JSON.stringify(res.anime_name), true)
+      .setImage(res.character_image);
+
+      message.channel.send({ embeds: [Embed] });
+
+    })
+      .catch((err) =>
+        message.channel.send({embeds:[
+          new MessageEmbed()
+            .setDescription(
+              `That character isn't found!\n\n\`\`\`js\n${err}\n\`\`\``
+            )
+            .setColor("#cc338b")
+        ]}
+        )
+      );
 };
 
 module.exports.help = {
@@ -25,4 +43,4 @@ module.exports.help = {
 	usage: "f-animecharacter <query>",
 	accessableby: "Member",
 	aliases: ["animecharactersearch"]
-};
+}; **/

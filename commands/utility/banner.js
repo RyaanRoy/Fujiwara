16390875519d31608c7e6df376f83c1e23dd9a02
Ryaan <73543787+RyaanRoy@users.noreply.github.com>
2022-01-13 20:15:36 +0530

    ok
    ok

--- commands/utility/banner.js
+++ commands/utility/banner.js
@@ -14,12 +14,12 @@ axios.get(`https://discord.com/api/users/${user.id}`,{
 
 	if(banner){
 const extension=banner.startsWith("a_") ? '.gif' : '.png';
-const url = `https://cdn.discordapp.com/banners/${user.id}/${banner}${extension}?size=4096`;
+const url = `https://cdn.discordapp.com/banners/${user.id}/${banner}${extension}`;
 	
 const embed = new Discord.MessageEmbed()
 .setTitle(`${user.tag}'s banner'`)
-.setImage(url)
-.setTimestamp();
+.setImage(`${url}?size=4096`)
+
 
 message.channel.send({embeds:[embed]});
 
